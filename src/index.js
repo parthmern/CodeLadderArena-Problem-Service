9c7a1e236edb59c09a505430a5072848d539472a
parthmern <parthmern@gmail.com>
2024-11-11 10:23:42 -0500

    cors fix

--- src/index.js
+++ src/index.js
@@ -10,10 +10,7 @@ const cookieParser = require("cookie-parser");
 const app = express();
 
 // app.use(cors());
-app.use(cors({
-    origin: ["*", CLIENTSIDE_URL, SUBMISSIONSERVICE_URL], // Dont mashedup with it
-    credentials: true, // Allow cookies to be sent across domains
-}));
+app.use(cors());
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
