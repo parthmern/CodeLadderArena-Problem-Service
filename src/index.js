fb8ec63efaefe1acb9691502acfec4850e8b129d
parthmern <parthmern@gmail.com>
2024-11-11 21:55:51 -0500

    Fix: clientside url

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const app = express();
 // app.use(cors({
 //     origin: ['http://localhost:3000', 'http://submissionservice.parthmern.store'], // List the exact origins
 //     credentials: true, // Allow cookies to be sent across domains
-//   }));
+//   })); 
 
 app.use(cors({
     origin: [CLIENTSIDE_URL, SUBMISSIONSERVICE_URL], // List the exact origins
